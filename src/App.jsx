82bd76b39b11894e7659bc4c0bba1fed58723360
keven-shtk <kevendicamargo@gmail.com>
2025-07-29 12:02:37 -0300

    feat: implementar temas claro e escuro

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import Header from "./components/Header";
 
-import './App.css'
+import { useState, useEffect } from "react";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    document.body.className = isDarkMode ? "dark-theme" : "light-theme";
+  }, [isDarkMode]);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
 
   return (
-    <>
-     
-    </>
-  )
+    <div>
+      <Header toggleTheme={toggleTheme} isDarkMode={isDarkMode} />
+    </div>
+  );
 }
 
-export default App
+export default App;
